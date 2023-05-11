5796af025962e394bc62b96fe3756d4f50b57a7b
Guillermo Martinez <gmartinez948@outlook.com>
2023-05-10 20:21:55 -0400

    css

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NameHeader from "./NameHeader";
+import "./App.css";
+import { motion, useScroll } from "framer-motion";
 
 function App() {
+  const { scrollYProgress } = useScroll();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div className="progress-bar" style={{ scaleX: scrollYProgress }}>
+      <div>
+        <NameHeader />
+      </div>
+    </motion.div>
   );
 }
 
---
+++ my-app/src/NameHeader.tsx
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+
+const NameHeader = () => {
+  return (
+    <motion.div
+      className="Header"
+      initial={{ opacity: 0, scale: 1 }}
+      animate={{ opacity: 1, x: 0, position: "fixed" }}
+      transition={{
+        duration: 2.0,
+        delay: 0.8,
+        ease: [0, 0.6, 0.2, 1.01],
+      }}
+    >
+      {" "}
+      <div className="Name-Text">
+        <p>
+          Hi, I'm Guillermo Martinez
+          <br /> Full Stack Software Engineer
+        </p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default NameHeader;
